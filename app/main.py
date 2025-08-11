7f5c50d67f38dda8d43235ca4ab7005b193d5117
Jeremiah-Williams1 <jerremiaholuwafemi@gmail.com>
2025-08-11 20:02:28 +0100

    Some other changes to app

--- app/main.py
+++ app/main.py
@@ -7,3 +7,13 @@ def home():
     return {'Message': 'Welcome to Churn Prediction',
             'Goto': '/predict root for prediction'
             }
+
+@app.post('/predict')
+def predict():
+    pass
+    # loads the encoders
+    # set the schema for the input to be set
+    # pass the input to the encoder to encdde it
+    # load the model
+    # pass the encoded input to the model
+    # get a prediction 
