2ff837d0e0e5c654e23b250fcce34269edb487cb
Jeremiah-Williams1 <jerremiaholuwafemi@gmail.com>
2025-08-29 00:17:05 +0100

    CI

--- airflow/dags/try.py
+++ airflow/dags/try.py
@@ -8,7 +8,7 @@ import os
 
 # Default arguments for the DAG
 default_args = {
-    'owner': 'Jerr-williams',
+    'owner': 'Jeremiah-williams',
     'start_date': datetime(2025, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
